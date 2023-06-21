b65ffa2e7839e376eed31f49440c546363141afe
Filipe Moscovo <128196275+Fmoscovo@users.noreply.github.com>
2023-06-21 14:01:12 -0600

    codeacademy

---
+++ Other training/test.js
@@ -0,0 +1,10 @@
+//What would the output of this block of code be?
+
+const animal = 'cat';
+ 
+for (let i = 0; i < animal.length; i++) {
+  console.log(animal[i]);
+  for (let j = 1; j < 4; j++) {
+    console.log(j);
+  }
+}
