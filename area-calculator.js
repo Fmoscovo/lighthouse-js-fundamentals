ae2e04f3e661234d987ea686652c634c67215af8
Filipe Moscovo <your@email.com>
2023-06-29 09:12:26 -0600

    asd

--- area-calculator.js
+++ area-calculator.js
@@ -21,4 +21,3 @@ function calculateCircleArea(radius) {
     return undefined;
   }
 }
-
---
+++ num-sum.js
@@ -0,0 +1,4 @@
+//write a program that takes in a an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers//
+
+
+console.log()
